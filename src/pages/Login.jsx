0825916804a0dc1f97b3d6800c08ead096013d1d
Vitor Silva <vitor.o.silva95@gmail.com>
2021-11-05 18:36:57 -0300

    Implementa requisitos 7 e 8

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,17 @@
+import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import Input from '../components/Input';
 
-function Login() {
+function Login({ history }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   function handleClick() {
+    const user = { email };
     localStorage.setItem('mealsToken', JSON.stringify(1));
     localStorage.setItem('cocktailsToken', JSON.stringify(1));
+    localStorage.setItem('user', JSON.stringify(user));
+    history.push('/comidas');
   }
 
   const isValidEmail = email.match(/\S+@\S+\.\S+/);
@@ -39,5 +43,11 @@ function Login() {
     </form>
   );
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;
