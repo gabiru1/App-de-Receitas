ae55214c15d27e77af174d9ab1df44b3ed7a6628
Alexandre Fortes <fortes04alexandre@gmail.com>
2021-11-09 20:49:14 -0300

    Requisitos 19 ao 24

--- src/pages/ExploreFood.jsx
+++ src/pages/ExploreFood.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function ExploreFood() {
   return (
     <div>
       <Header title="Explorar Comidas" showSearchBtn={ false } />
+      <Footer />
     </div>
   );
 }
