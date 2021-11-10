f36b83569e4e71fa1ef9bfc4c6554b2f7f17ef9f
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-11-10 19:06:11 -0300

    Impl. req 70 ao 73

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-function ExploreDrinks() {
+function ExploreDrinks({ history }) {
   return (
     <div>
       <Header title="Explorar Bebidas" showSearchBtn={ false } />
+      <div>
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+          onClick={ () => history.push('/explorar/bebidas/ingredientes') }
+        >
+          Por Ingredientes
+        </button>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+        >
+          Me Surpreenda!
+        </button>
+      </div>
       <Footer />
     </div>
   );
 }
+
+ExploreDrinks.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default ExploreDrinks;
--- src/pages/ExploreFood.jsx
+++ src/pages/ExploreFood.jsx
@@ -1,14 +1,43 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-function ExploreFood() {
+function ExploreFood({ history }) {
   return (
     <div>
       <Header title="Explorar Comidas" showSearchBtn={ false } />
+      <div>
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+          onClick={ () => history.push('/explorar/comidas/ingredientes') }
+        >
+          Por Ingredientes
+        </button>
+        <button
+          type="button"
+          data-testid="explore-by-area"
+          onClick={ () => history.push('/explorar/comidas/area') }
+        >
+          Por Local de Origem
+        </button>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+        >
+          Me Surpreenda!
+        </button>
+      </div>
       <Footer />
     </div>
   );
 }
+
+ExploreFood.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default ExploreFood;
