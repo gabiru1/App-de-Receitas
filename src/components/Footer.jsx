ecc66131efa0dd39301601aef2eb1e5dcff35319
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-11-18 15:13:20 -0300

    Estiliza pagina de perfil

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,25 +6,17 @@ import exploreIcon from '../images/exploreIcon.svg';
 
 function Footer() {
   return (
-
     <footer className="footer" data-testid="footer">
       <Link to="/explorar">
-        <button type="button" data-testid="explore-bottom-btn" src={ exploreIcon }>
-          <img src={ exploreIcon } alt="Explore Icon" />
-        </button>
+        <img data-testid="explore-bottom-btn" src={ exploreIcon } alt="Explore Icon" />
       </Link>
       <Link to="/comidas">
-        <button type="button" data-testid="food-bottom-btn" src={ mealIcon }>
-          <img src={ mealIcon } alt="Meal Icon" />
-        </button>
+        <img data-testid="food-bottom-btn" src={ mealIcon } alt="Meal Icon" />
       </Link>
       <Link to="/bebidas">
-        <button type="button" data-testid="drinks-bottom-btn" src={ drinkIcon }>
-          <img src={ drinkIcon } alt="Drink Icon" />
-        </button>
+        <img data-testid="drinks-bottom-btn" src={ drinkIcon } alt="Drink Icon" />
       </Link>
     </footer>
-
   );
 }
 
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import Footer from '../components/Footer';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Profile({ history }) {
   const [email, setEmail] = useState('');
 
   function getEmail() {
-    const userEmail = JSON.parse(localStorage.getItem('user')).email;
-    setEmail(userEmail);
+    if (localStorage.getItem('user')) {
+      const userEmail = JSON.parse(localStorage.getItem('user')).email;
+      setEmail(userEmail);
+    }
   }
 
   function handleLogout() {
@@ -21,36 +23,40 @@ function Profile({ history }) {
   }, []);
 
   return (
-    <div>
+    <section>
       <Header title="Perfil" showSearchBtn={ false } />
-      <div className="container-profile-email">
-        <h2 data-testid="profile-email">{email}</h2>
-      </div>
-      <div className="container-buttons-profile">
-        <button
-          type="button"
-          data-testid="profile-done-btn"
-          onClick={ () => history.push('/receitas-feitas') }
-        >
-          Receitas Feitas
-        </button>
-        <button
-          type="button"
-          data-testid="profile-favorite-btn"
-          onClick={ () => history.push('/receitas-favoritas') }
-        >
-          Receitas Favoritas
-        </button>
-        <button
-          type="button"
-          data-testid="profile-logout-btn"
-          onClick={ handleLogout }
-        >
-          Sair
-        </button>
+      <div>
+        <div className="container-profile-email">
+          { email && (
+            <h2 data-testid="profile-email">{email}</h2>
+          )}
+        </div>
+        <div className="container-buttons-profile">
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+            onClick={ () => history.push('/receitas-feitas') }
+          >
+            Receitas Feitas
+          </button>
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+            onClick={ () => history.push('/receitas-favoritas') }
+          >
+            Receitas Favoritas
+          </button>
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+            onClick={ handleLogout }
+          >
+            Sair
+          </button>
+        </div>
       </div>
       <Footer />
-    </div>
+    </section>
   );
 }
 
