1a6a60a6578235751e3d441d1b6be902fa7089a5
Vitor Silva <vitor.o.silva95@gmail.com>
2021-11-19 19:44:35 -0300

    Implementa logica de favoritar

--- src/pages/DrinkInProgress.jsx
+++ src/pages/DrinkInProgress.jsx
@@ -4,6 +4,8 @@ import { useParams } from 'react-router';
 import './FoodInProgress.css';
 import getIngredients from '../helper/helper';
 import { fetchApiByID } from '../services/FetchApi';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 
 function DrinkInProgress({ history }) {
   const [fullIngredient, setFullIngredient] = useState([]);
@@ -11,6 +13,8 @@ function DrinkInProgress({ history }) {
   const [ingredientsList, setIngredientsList] = useState([]);
   const [getLocal, setLocal] = useState([]);
   const [recipe, setRecipe] = useState({});
+  const [heart, setHeart] = useState(whiteHeartIcon);
+  const [toggleHeart, setToggleHeart] = useState(true);
   const { recipeId } = useParams();
 
   function getFullIngredients(response) {
@@ -88,6 +92,10 @@ function DrinkInProgress({ history }) {
   useEffect(() => {
     fetchApi();
     sendLocalStorage();
+    const exist = localStorage.getItem('favoriteRecipes');
+    if (exist) {
+      setHeart(blackHeartIcon);
+    }
   }, []);
 
   function getDate() {
@@ -122,6 +130,39 @@ function DrinkInProgress({ history }) {
       localStorage.setItem('doneRecipes', JSON.stringify([...json, doneRecipe]));
     } else localStorage.setItem('doneRecipes', JSON.stringify([doneRecipe]));
     history.push('/receitas-feitas');
+  }
+
+  function setFavoriteInLocalStorage() {
+    const exist = localStorage.getItem('favoriteRecipes');
+    const obj = {
+      id: recipeId,
+      type: 'bebida',
+      area: recipe.strArea,
+      category: recipe.strCategory,
+      alcoholicOrNot: recipe.strAlcoholic,
+      name: recipe.strDrink,
+      image: recipe.strDrinkThumb,
+    };
+    console.log(obj);
+
+    if (exist) {
+      const json = JSON.parse(exist);
+      localStorage.setItem('favoriteRecipes', JSON.stringify([...json, obj]));
+      return;
+    }
+    localStorage.setItem('favoriteRecipes', JSON.stringify([obj]));
+  }
+  function handleFavorite() {
+    setToggleHeart(!toggleHeart);
+    if (toggleHeart) {
+      setHeart(blackHeartIcon);
+      setFavoriteInLocalStorage();
+    } else {
+      setHeart(whiteHeartIcon);
+      const getLocalStorage = JSON.parse(localStorage.getItem('favoriteRecipes'));
+      const filtered = getLocalStorage.filter((element) => element.id !== recipeId);
+      localStorage.setItem('favoriteRecipes', JSON.stringify(filtered));
+    }
   }
 
   return (
@@ -160,7 +201,7 @@ function DrinkInProgress({ history }) {
         {recipe.strInstructions }
       </div>
       <button type="button" data-testid="share-btn">compartilhar</button>
-      <button type="button" data-testid="favorite-btn">favoritar</button>
+      <button type="button" onClick={ handleFavorite }><img data-testid="favorite-btn" src={ heart } alt="favoritar" /></button>
       <button
         type="button"
         data-testid="finish-recipe-btn"
