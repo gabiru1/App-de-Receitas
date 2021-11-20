84191f3c21bc969c4a2a231c6dc0e16becd2847d
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-11-20 20:57:41 -0300

    Componetiza função de salvar localStorage receitas feitas

--- src/pages/DrinkInProgress.jsx
+++ src/pages/DrinkInProgress.jsx
@@ -6,6 +6,7 @@ import getIngredients from '../helper/helper';
 import { fetchApiByID } from '../services/FetchApi';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import saveDrinkRecipeLocalStorage from '../helper/saveDrinkRecipeLocalStorage';
 
 function DrinkInProgress({ history }) {
   const [fullIngredient, setFullIngredient] = useState([]);
@@ -98,38 +99,8 @@ function DrinkInProgress({ history }) {
     }
   }, []);
 
-  function getDate() {
-    const data = new Date();
-    const dia = String(data.getDate()).padStart(2, '0');
-    const mes = String(data.getMonth() + 1).padStart(2, '0');
-    const ano = data.getFullYear();
-    const dataAtual = `${dia}/${mes}/${ano}`;
-    return dataAtual;
-  }
-
   function handleFinish() {
-    const exist = localStorage.getItem('doneRecipes');
-    let tags = [];
-    if (!recipe.strTags === ('' || null)) {
-      tags = recipe.strTags;
-    }
-    const doneRecipe = {
-      id: recipeId,
-      type: 'bebida',
-      area: recipe.strArea,
-      category: recipe.strCategory,
-      alcoholicOrNot: recipe.strAlcoholic,
-      name: recipe.strDrink,
-      image: recipe.strDrinkThumb,
-      doneDate: getDate(),
-      tags,
-    };
-
-    if (exist) {
-      const json = JSON.parse(exist);
-      localStorage.setItem('doneRecipes', JSON.stringify([...json, doneRecipe]));
-    } else localStorage.setItem('doneRecipes', JSON.stringify([doneRecipe]));
-    history.push('/receitas-feitas');
+    saveDrinkRecipeLocalStorage(recipe, recipeId, 'bebida', history);
   }
 
   function setFavoriteInLocalStorage() {
--- src/pages/FoodInProgress.jsx
+++ src/pages/FoodInProgress.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router';
 import { fetchApiByID } from '../services/FetchApi';
 import './FoodInProgress.css';
 import getIngredients from '../helper/helper';
+import saveFoodRecipeLocalStorage from '../helper/saveFoodRecipeLocalStorage';
 
 function FoodInProgress({ history }) {
   const [fullIngredient, setFullIngredient] = useState([]);
@@ -95,37 +96,8 @@ function FoodInProgress({ history }) {
     sendLocalStorage();
   }, []);
 
-  function getDate() {
-    const data = new Date();
-    const dia = String(data.getDate()).padStart(2, '0');
-    const mes = String(data.getMonth() + 1).padStart(2, '0');
-    const ano = data.getFullYear();
-    const dataAtual = `${dia}/${mes}/${ano}`;
-    return dataAtual;
-  }
-
   function handleFinish() {
-    const exist = localStorage.getItem('doneRecipes');
-    let tags = [];
-    if (!recipe.strTags === ('' || null)) {
-      tags = recipe.strTags;
-    }
-    const doneRecipe = {
-      id: recipeId,
-      type: 'comida',
-      area: recipe.strArea,
-      category: recipe.strCategory,
-      alcoholicOrNot: '',
-      name: recipe.strMeal,
-      image: recipe.strMealThumb,
-      doneDate: getDate(),
-      tags,
-    };
-    if (exist) {
-      const json = JSON.parse(exist);
-      localStorage.setItem('doneRecipes', JSON.stringify([...json, doneRecipe]));
-    } else localStorage.setItem('doneRecipes', JSON.stringify([doneRecipe]));
-    history.push('/receitas-feitas');
+    saveFoodRecipeLocalStorage(recipe, recipeId, 'comida', history);
   }
 
   return (
